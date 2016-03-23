c214c982806b164e62436db218c4875b517a859a
Aleksey Bakin <alekseybakin@mylnikta.corp.lukoil.com>
2016-03-23 21:28:08 +0500

    first commit

---
+++ adiptivnuisite/index.php
@@ -0,0 +1,133 @@
+<!DOCTYPE HTML>
+<html class="no-js" lang="en">
+<head>
+
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<title>Адаптивный сайт</title>
+<link rel="stylesheet" href="css/style.css" />
+<link rel="stylesheet" href="css/style1.css" media="screen and (max-width:621px)" />
+<link rel="stylesheet" href="css/flexiblemedia.css" />
+<script type="text/javascript" src="js/picturefill.js"></script>
+<script type="text/javascript" src="js/matchmedia.js"></script>
+
+</head>
+
+<body>
+<div class="container">
+ <div class="row">
+  <div class="cols col-12">
+  <h1>Заголовок</h1>
+  </div>
+   <div class="cols col-4">
+   <h2>Позаголовок</h2>
+   <p>
+“My feet they are sore, and my limbs they are weary;
+   Long is the way, and the mountains are wild;
+Soon will the twilight close moonless and dreary
+   Over the path of the poor orphan child.
+
+Why did they send me so far and so lonely,
+   Up where the moors spread and grey rocks are piled?
+Men are hard-hearted, and kind angels only
+   Watch o’er the steps of a poor orphan child.
+
+Yet distant and soft the night breeze is blowing,
+   Clouds there are none, and clear stars beam mild,
+God, in His mercy, protection is showing,
+   Comfort and hope to the poor orphan child.
+
+</p>
+   </div>
+   <div class="cols col-4">
+      <h2>Позаголовок</h2>
+   <p>
+“My feet they are sore, and my limbs they are weary;
+   Long is the way, and the mountains are wild;
+Soon will the twilight close moonless and dreary
+   Over the path of the poor orphan child.
+
+Why did they send me so far and so lonely,
+   Up where the moors spread and grey rocks are piled?
+Men are hard-hearted, and kind angels only
+   Watch o’er the steps of a poor orphan child.
+
+Yet distant and soft the night breeze is blowing,
+   Clouds there are none, and clear stars beam mild,
+God, in His mercy, protection is showing,
+   Comfort and hope to the poor orphan child.
+
+</p>
+   
+   </div>
+   <div class="cols col-4"> 
+   
+      <h2>Позаголовок</h2>
+   <p>
+“My feet they are sore, and my limbs they are weary;
+   Long is the way, and the mountains are wild;
+Soon will the twilight close moonless and dreary
+   Over the path of the poor orphan child.
+
+Why did they send me so far and so lonely,
+   Up where the moors spread and grey rocks are piled?
+Men are hard-hearted, and kind angels only
+   Watch o’er the steps of a poor orphan child.
+
+Yet distant and soft the night breeze is blowing,
+   Clouds there are none, and clear stars beam mild,
+God, in His mercy, protection is showing,
+   Comfort and hope to the poor orphan child.
+
+</p>
+   
+   
+   
+   </div>
+   </div>
+   <div class="row">
+<div class="cols col-3">Три</div>
+<div class="cols col-3">Три</div>  
+<div class="cols col-3">Три</div>   
+<div class="cols col-3">Три</div>
+</div>
+
+<div class="row">
+ <div class="cols col-12">
+ <h1>Привет</h1>
+ 
+ <figure>
+ <span data-picture data-alt="a giant stone face at the Bayon temple
+ in Angkor Thom, Cambodia">
+ <span data-src="small.jpg"></span>
+ <span data-src="medium.jpg" data-media="(min-width:400px;)"></span>
+  <span data-src="large.jpg" data-media="(min-width:800px;)"></span>
+
+ <span data-src="extralarge.jpg" data-media="(min-width:1000px;)"></span>
+<noscript>
+<img  src="ava-s.jpg" alt="A giant stone face at The Bayon temple in
+Canbodia"/>
+
+</noscript>
+ </span>
+ 
+ 
+ </figure>
+ <img src="img/babochka132.gif"/>
+ </div>
+</div>
+<div class="row">
+ <div class="cols col-3">three</div>
+ <div class="cols col-7">Seven</div>
+ <div class="cols col-2">too</div>
+</div>
+
+
+</div>
+</body>
+
+
+
+
+
+</html>
